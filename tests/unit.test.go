b5f4d5bbe21af5db817c0c5ff750544319727c8b
Anusorn <anusondd@gmail.com>
2021-01-18 04:24:13 +0700

    action push image stop

--- tests/unit.test.go
+++ tests/unit.test.go
@@ -2,13 +2,11 @@ package tests
 
 import (
 	"fmt"
-	"go-api/services"
 )
 
 func UnitTest() {
 	fmt.Println("Start Test==========================")
-	conn := services.ExecuteCmd("docker ps", "0.0.0.0", "22", "user", "pass")
-	// result := conn.Cmd("docker ps")
-	fmt.Println("Out : %s", conn)
+	// conn := services.ExecuteCmd("docker ps", "0.0.0.0", "22", "user", "pass")
+	// fmt.Println("Out : %s", conn)
 	fmt.Println("Test End==========================")
 }
